69a0a182e38c4adfcb1dddb2ed505d6d1b551b77
Darryl Dave de Castro <ddecastro@yondu.com>
2022-01-18 11:16:06 +0800

    Commit for Layouts - Added color, dimension values - Created shapes and background drawables - Added Image assets - Created Main layout - Create Recyclerview items layout - Enabled databinding
    Fixed Banner Texts to cater dynamic layouts

--- app/src/main/java/com/quantrics/guidomia/MainActivity.kt
+++ app/src/main/java/com/quantrics/guidomia/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.quantrics.guidomia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.quantrics.guidomia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
     }
 }
